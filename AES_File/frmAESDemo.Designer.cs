e04d326ca9a0a47ba9acb0f24ad622956c7278f7
HieuPhamBa <45411070+HieuPhamBa@users.noreply.github.com>
2020-12-07 07:54:15 +0700

    Aes file

---
+++ AES_File/frmAESDemo.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace AES_File
+{
+    partial class frmAESDemo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.rdEncrypt = new System.Windows.Forms.RadioButton();
+            this.rdDecypt = new System.Windows.Forms.RadioButton();
+            this.btnExcute = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File path";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(31, 48);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(327, 27);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(364, 48);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(94, 29);
+            this.btnBrowser.TabIndex = 2;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // rdEncrypt
+            // 
+            this.rdEncrypt.AutoSize = true;
+            this.rdEncrypt.Location = new System.Drawing.Point(31, 95);
+            this.rdEncrypt.Name = "rdEncrypt";
+            this.rdEncrypt.Size = new System.Drawing.Size(104, 24);
+            this.rdEncrypt.TabIndex = 3;
+            this.rdEncrypt.TabStop = true;
+            this.rdEncrypt.Text = "Encrypt file";
+            this.rdEncrypt.UseVisualStyleBackColor = true;
+            this.rdEncrypt.CheckedChanged += new System.EventHandler(this.rdEncrypt_CheckedChanged);
+            // 
+            // rdDecypt
+            // 
+            this.rdDecypt.AutoSize = true;
+            this.rdDecypt.Location = new System.Drawing.Point(217, 95);
+            this.rdDecypt.Name = "rdDecypt";
+            this.rdDecypt.Size = new System.Drawing.Size(107, 24);
+            this.rdDecypt.TabIndex = 4;
+            this.rdDecypt.TabStop = true;
+            this.rdDecypt.Text = "Decrypt file";
+            this.rdDecypt.UseVisualStyleBackColor = true;
+            this.rdDecypt.CheckedChanged += new System.EventHandler(this.rdDecypt_CheckedChanged);
+            // 
+            // btnExcute
+            // 
+            this.btnExcute.Location = new System.Drawing.Point(31, 172);
+            this.btnExcute.Name = "btnExcute";
+            this.btnExcute.Size = new System.Drawing.Size(427, 29);
+            this.btnExcute.TabIndex = 5;
+            this.btnExcute.Text = "Start";
+            this.btnExcute.UseVisualStyleBackColor = true;
+            this.btnExcute.Click += new System.EventHandler(this.btnExcute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(107, 129);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(251, 27);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // frmAESDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 222);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExcute);
+            this.Controls.Add(this.rdDecypt);
+            this.Controls.Add(this.rdEncrypt);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAESDemo";
+            this.Text = "Aes";
+            this.Load += new System.EventHandler(this.frmAESDemo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.RadioButton rdEncrypt;
+        private System.Windows.Forms.RadioButton rdDecypt;
+        private System.Windows.Forms.Button btnExcute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ AES_File/frmAESDemo.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES_File
+{
+    public partial class frmAESDemo : Form
+    {
+        byte[] abc;
+        byte[,] table;
+
+        public frmAESDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAESDemo_Load(object sender, EventArgs e)
+        {
+            rdEncrypt.Checked = true;
+
+            // init abc and table
+            abc = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                abc[i] = Convert.ToByte(i);
+            }
+
+            table = new byte[256, 256];
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    table[i, j] = abc[(i + j) % 256];
+                }
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = od.FileName;
+            }
+        }
+
+        private void rdEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdEncrypt.Checked)
+            {
+                rdDecypt.Checked = false;
+            }
+        }
+
+        private void rdDecypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdDecypt.Checked)
+            {
+                rdEncrypt.Checked = false;
+            }
+        }
+
+        private void btnExcute_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("File does not exist.");
+                return;
+            }
+            if (String.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Password empty. Please enter your password");
+                return;
+            }
+
+            // Get file content and key for encrypt/decrypt
+            try
+            {
+                byte[] fileContent = File.ReadAllBytes(txtFilePath.Text);
+                byte[] passwordTmp = Encoding.ASCII.GetBytes(txtPassword.Text);
+                byte[] keys = new byte[fileContent.Length];
+                for (int i = 0; i < fileContent.Length; i++)
+                    keys[i] = passwordTmp[i % passwordTmp.Length];
+
+                // Encrypt
+                byte[] result = new byte[fileContent.Length];
+
+                if (rdEncrypt.Checked)
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        result[i] = table[keyIndex, valueIndex];
+                    }
+                }
+                // Decrypt
+                else
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (table[keyIndex, j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        result[i] = abc[valueIndex];
+                    }
+                }
+
+                // Save result to new file with the same extension
+                String fileExt = Path.GetExtension(txtFilePath.Text);
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Files (*" + fileExt + ") | *" + fileExt;
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(sd.FileName, result);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("File is in use.Close other program is using this file and try again.");
+                return;
+            }
+        }
+    }
+}
